269de690237869e4326e32c0d371ba8b94542b2c
Mohammad RN <mr.nezamdust@gmail.com>
2024-11-18 17:38:20 +0330

    new fonts addeed

---
+++ src/layouts/main/components/footer/index.tsx
@@ -0,0 +1,15 @@
+/**
+ * Footer component
+ * @returns {JSX.Element} Footer component
+ */
+export default function Footer() {
+  return (
+    <div className="border-t py-6 mt-10 bg-white">
+      <div className="container mx-auto">
+        <p className="text-xs text-zinc-400 font-geist-mono">
+          © 2024 Bettermode Community Platform. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -6,12 +6,14 @@
  */
 import { ReactNode } from "react";
 import Header from "./components/header";
+import Footer from "./components/footer";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
     <div className="bg-zinc-100 pt-32">
       <Header />
       {children}
+      <Footer/>
     </div>
   );
 }
