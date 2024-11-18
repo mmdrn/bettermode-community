298a39a03aa11fd2697d3057b344d4ac42886c48
Mohammad RN <mr.nezamdust@gmail.com>
2024-11-18 16:50:49 +0330

    header improved

--- src/layouts/main/components/header/index.tsx
+++ src/layouts/main/components/header/index.tsx
@@ -1,19 +1,44 @@
+import { Bold, CodeXml, LogIn } from "lucide-react";
+import { Link } from "react-router-dom";
+import logo from "./../../../../assets/logo.svg";
+
 /**
- * Header component that displays a title with an emoji icon
- * @param {Object} props - Component props
- * @param {string} props.title - The title text to display in the header
- * @returns {JSX.Element} Header component with title and emoji
+ * Header component that displays the logo
+ * @returns {JSX.Element} Header component with logo
  */
-export default function Header({ title }: { title: string }) {
+export default function Header() {
   return (
-    <div className="border-b p-2 mb-6">
-      <div className="container mx-auto">
-        <h1 className="font-bold text-2xl font-mono inline-flex items-center justify-start gap-3">
-          <span className="text-3xl w-11 h-11 rounded-md bg-blue-500 flex items-center justify-center">
-            🥸
-          </span>
-          {title}
-        </h1>
+    <div className="border-b py-6 mb-6">
+      <div className="container mx-auto flex justify-between items-center">
+        <img
+          src={logo}
+          alt="Bettermode Community"
+          title="Bettermode Community Logo"
+          className="w-56"
+        />
+
+        <div className="flex items-center justify-end flex-row-reverse gap-5">
+          <Link
+            to={"/signin"}
+            className="flex items-center justify-start gap-2 px-1 hover:text-bettermode-green-primary"
+          >
+            Signin
+          </Link>
+
+          <Link
+            to={"/signin"}
+            className="flex items-center justify-start gap-2 px-1 hover:text-bettermode-green-primary"
+          >
+            Github
+          </Link>
+
+          <Link
+            to={"/signin"}
+            className="flex items-center justify-start gap-2 px-1 hover:text-bettermode-green-primary"
+          >
+            Documents
+          </Link>
+        </div>
       </div>
     </div>
   );
