6d9478cd7e02f4859f8a3fd158fe994dd705268a
Mohammad RN <mr.nezamdust@gmail.com>
2024-11-27 11:35:20 +0330

    testing lib configure

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -4,7 +4,12 @@ export default function Loading({ className }: { className?: string }) {
   return (
     <div className={`col-span-4 flex justify-center ${className || ""}`}>
       <p className="text-bettermode-green-primary flex items-center justify-center h-[42px] w-full gap-2 font-geist-mono font-semibold">
-        <LoaderCircle className="animate-spin" strokeWidth={2.6} size={30} />
+        <LoaderCircle
+          data-testid="loader-icon"
+          className="animate-spin"
+          strokeWidth={2.6}
+          size={30}
+        />
         Loading data...
       </p>
     </div>
